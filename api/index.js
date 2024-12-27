ee024c7d64691ad54686b5b366d5cab14c638b17
Antim <2021ume0204@iitjammu.ac.in>
2024-12-27 13:16:00 +0530

    create_signup_api_route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
 dotenv.config();
 
 
@@ -10,6 +11,8 @@ mongoose.connect(process.env.MONGO).
 then(()=>console.log('MongoDB connected'))
 .catch(error=>console.log(error));
 const app=express();
+
+app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.json({
@@ -25,3 +28,4 @@ console.log(`Server running on port 3000`);
 );  
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
